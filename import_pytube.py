ca867bc0e5aef1382e24ce25f8094f4a3ee3221e
Dimitar Dimitrov <mitko.69@gmail.com>
2023-08-17 01:15:35 +0200

    add error checking

--- import_pytube.py
+++ import_pytube.py
@@ -1,15 +1,29 @@
-"""Imports the pytube module to allow downloading YouTube videos."""
+"""
+Imports the pytube module to allow downloading YouTube videos.
 
+To run this:
+python program.py https://www.youtube.com/watch?v=VIDEO_ID
+"""
+
+import sys
 import pytube
 
-# Download video
-yt = pytube.YouTube("https://www.youtube.com/watch?v=6yQEA18C-XI")
-video = yt.streams.filter(only_audio=True).first()
-out_file = video.download(output_path="./")
+if len(sys.argv) != 2:
+    print("Error: Incorrect number of arguments")
+    print(f"Usage: python {sys.argv[0]} <video_url>")
+    sys.exit(1)
 
-# Copy file to iPhone 
-#iphone_music_folder = "/path/to/iphone/music/folder"
-#filename = Path(out_file).name
-#shutil.copy(out_file, iphone_music_folder + "/" + filename)
+# Download video
+VIDEO_URL = sys.argv[1]
+try:
+    yt = pytube.YouTube(VIDEO_URL)
+except Exception as e:
+    print(f"Error: Video unavailable or invalid URL - {VIDEO_URL}", e)
+    sys.exit(1)
+
+video = yt.streams.filter(only_audio=False).first()
+# for high-res video:
+#video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+out_file = video.download(output_path="./")
 
 print(f"Downloaded {video.title}")
