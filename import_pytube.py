c977536cd1569d8b085339af25c3277cf2d2c62b
Dimitar Dimitrov <mitko.69@gmail.com>
2023-08-17 00:25:00 +0200

    initial

---
+++ import_pytube.py
@@ -0,0 +1,15 @@
+#from pathlib import Path
+#import shutil
+import pytube
+
+# Download video
+yt = pytube.YouTube("https://www.youtube.com/watch?v=6yQEA18C-XI")
+video = yt.streams.filter(only_audio=True).first()
+out_file = video.download(output_path="./")
+
+# Copy file to iPhone 
+#iphone_music_folder = "/path/to/iphone/music/folder"
+#filename = Path(out_file).name
+#shutil.copy(out_file, iphone_music_folder + "/" + filename)
+
+print(f"Downloaded {video.title}")
